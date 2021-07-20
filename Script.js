9296cc6dbffaabfcd4b84c20108547449f5d4f42
Riley Lum <rileylum@adam.com.au>
2021-07-20 17:15:13 +0930

    update createRedditPost function

--- Script.js
+++ Script.js
@@ -1,28 +1,22 @@
-var htmlBody = document.querySelector('body');
-var redditInput =document.querySelector('#search-bar');
-var searchRedditBtn =document.querySelector("#search-sub-reddit");
-
+var postSection = document.querySelector('#results');
 
 //Following code should happen on the button click event
 //For now, I am using dummy data
-var searchTerm="Adelaide";
+var searchTerm = "Adelaide";
 
-if (searchTerm) 
-{
+if (searchTerm) {
     getNewListings(searchTerm);
-    searchTerm="";
-} 
+    searchTerm = "";
+}
 
-else 
-{
+else {
     window.alert("Please enter something to search.")
 }
 
 //########## This function is used to get the new 10 listings of the given subreddit##############
-function getNewListings(searchTerm)
-{
-    var fetchUrl="https://www.reddit.com/r/"+searchTerm+"/new.json?limit=10";
-       
+function getNewListings(searchTerm) {
+    var fetchUrl = "https://www.reddit.com/r/" + searchTerm + "/new.json?limit=10";
+
     fetch(fetchUrl)
         .then(function (response) {
             if (response.ok) {
@@ -30,49 +24,83 @@ function getNewListings(searchTerm)
                     displayListings(data);
                     console.log(data);
                 });
-            } 
-            
-            else if(response.status===404){
+            }
+
+            else if (response.status === 404) {
                 Window.alert("This does not exist");
             }
-            })
-                .catch(function (error) {
-                    alert('Cannot connect to Reddit');
-            });          
-            
+        })
+        .catch(function (error) {
+            alert('Cannot connect to Reddit');
+        });
+
 }
 
-//########## This function is used to display the new listings ##############
-function displayListings (listings) 
-{
-    if (listings.data.children.length === 0) 
-    {
-      console.log("Nothing found to display for this search term.");
-      return;
-    }
-    
-    else
-    {
-        for (var i = 0; i < listings.data.children.length; i++) 
-        {
-            var listingTitle =listings.data.children[i].data.title;
-            var listingContent = listings.data.children[i].data.selftext;
-            var listingAuthor = listings.data.children[i].data.author;
-            var listingUrl =listings.data.children[i].data.url;
-            var listingUpdated=moment(listings.data.children[i].data.created,"X").format("dddd, MMMM Do YYYY, h:mm:ss a")
-           
-            //var listingMedia= 
-           
-            console.log(listingTitle);
-            console.log(listingContent); 
-            console.log(listingAuthor);
-            console.log(listingUrl);
-            console.log(listingUpdated);
-    }
-    }   
-  };
+// //########## This function is used to display the new listings ##############
+function displayListings(listings) {
+    if (listings.data.children.length === 0) {
+        console.log("Nothing found to display for this search term.");
+        return;
+    } else {
+        for (var i = 0; i < listings.data.children.length; i++) {
+            listings.data.children[i].data.created = moment(listings.data.children[i].data.created_utc, 'X').format("dddd, MMMM Do YYYY, h:mm:ss a");
+            createRedditPost(listings.data.children[i].data);
 
-  //I did not find any media urls yet.
+            //     var listingTitle = listings.data.children[i].data.title;
+            //     var listingContent = listings.data.children[i].data.selftext;
+            //     var listingAuthor = listings.data.children[i].data.author;
+            //     var listingUrl = listings.data.children[i].data.url;
+            //     var listingUpdated = moment(listings.data.children[i].data.created, "X").format("dddd, MMMM Do YYYY, h:mm:ss a")
+
+            //     //var listingMedia= 
+
+            //     console.log(listingTitle);
+            //     console.log(listingContent);
+            //     console.log(listingAuthor);
+            //     console.log(listingUrl);
+            //     console.log(listingUpdated);
+            // }
+        }
+    }
+};
+
+//   //I did not find any media urls yet.
+
+function createRedditPost(data) {
+    // create container for reddit post
+    var redditPost = document.createElement('div');
+    redditPost.classList = "box"
+    var redditTitle = document.createElement('h2');
+    redditTitle.textContent = data.title.replace(/&amp;/g, '&');
+    redditTitle.classList = "title is-4";
+    var redditInfo = document.createElement('p');
+    redditInfo.textContent = "Posted by - " + data.author + ' @ ' + data.created;
+    redditInfo.classList = "tag subtitle is-6";
+    var redditLink = document.createElement('a');
+    redditLink.textContent = data.permalink;
+    redditLink.setAttribute('href', 'https://www.reddit.com' + data.permalink);
+    redditLink.setAttribute('target', '_blank');
+    redditPost.appendChild(redditTitle);
+    redditPost.appendChild(redditInfo);
+    redditPost.appendChild(redditLink);
+
+
+    if (data.selftext) {
+        var redditTextContent = document.createElement('p');
+        redditTextContent.innerHTML = data.selftext.replace(/&amp;/g, '&');
+        redditPost.appendChild(redditTextContent);
+    }
+
+    if (data.preview) {
+        console.log("hello");
+        var redditPreview = document.createElement('img');
+        redditPreview.setAttribute('src', data.preview.images[0].source.url.replace(/&amp;/g, '&'));
+        redditPreview.setAttribute('style', 'width: 50%');
+        redditPost.appendChild(redditPreview);
+    }
+
+    postSection.appendChild(redditPost);
+};
 
 
 function addCollapseListener() {
@@ -106,36 +134,3 @@ function collapseForm(e) {
 }
 
 addCollapseListener();
-
-dummyData = {
-    title: "Test title",
-    content: "I am test text-content",
-    author: "I am the test author",
-    url: "www.reddit.com/r/test/test",
-    mediaurls: ['https://preview.redd.it/09u6hmoj25b71.jpg?width=3024&format=pjpg&auto=webp&s=dc8bd26c15cb3049955c439b758eb9fd09e33b20'],
-    created: '9am'
-};
-
-function createRedditPost(data) {
-    var redditPost = document.createElement('div');
-    var redditTitle = document.createElement('h2');
-    redditTitle.textContent = data.title;
-    var redditInfo = document.createElement('p');
-    redditInfo.textContent = data.author + ' ' + data.url + ' ' + data.created;
-    redditPost.appendChild(redditTitle);
-    redditPost.appendChild(redditInfo);
-    if (data.content) {
-        var redditTextContent = document.createElement('p');
-        redditTextContent.textContent = data.content;
-        redditPost.appendChild(redditTextContent);
-    }
-    if (data.mediaurls) {
-        var redditImg = document.createElement('img');
-        redditImg.setAttribute('src', data.mediaurls[0]);
-        redditImg.setAttribute('style', 'width: 500px;');
-        redditPost.appendChild(redditImg);
-    }
-    htmlBody.appendChild(redditPost);
-};
-
-// createRedditPost(dummyData);
