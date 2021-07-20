c180ba6e7298f5f19b2851c4db6cbe0469543bbd
akashgithuboot <85092610+akashgithuboot@users.noreply.github.com>
2021-07-20 19:29:25 +0930

    //add addEventlistner

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 var htmlBody = document.querySelector('body');
 var redditInput =document.querySelector('#search-bar');
 var searchRedditBtn =document.querySelector("#search-sub-reddit");
-
+var subredditSearchButton = document.getElementById('subreddit-search-form');
 
 //Following code should happen on the button click event
 //For now, I am using dummy data
@@ -139,3 +139,25 @@ function createRedditPost(data) {
 };
 
 // createRedditPost(dummyData);
+
+//for the Raddit search submit button
+
+var searchRedditButton =document.querySelector("#reddit-search-form");
+var subredditSearchButton = document.getElementById('subreddit-search-form');
+
+
+searchRedditButton.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var searchTerm = document.getElementById("searchTerm").value;
+    console.log(searchTerm);
+ });
+
+ //for the Subraddit search submit button
+
+ subredditSearchButton.addEventListener('click',function(event){
+    event.preventDefault()
+
+    var SubredditName = document.getElementById("SubredditName").value
+    console.log(SubredditName)
+ });
